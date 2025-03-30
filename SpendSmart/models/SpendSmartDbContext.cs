38d28d0a1312daf5e14d564b9df1558edfc6a0e4
Sohail9403 <sohailrahil995@gmail.com>
2025-03-30 21:20:25 +0530

    Add files via upload

---
+++ SpendSmart/models/SpendSmartDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SpentSmart.Models
+{
+    public class SpendSmartDbContext : DbContext
+    {
+        public SpendSmartDbContext(DbContextOptions<SpendSmartDbContext> options) : base(options) { }
+
+        public DbSet<Expense> Expenses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Expense>()
+                .Property(e => e.Value)
+                .HasColumnType("decimal(18,2)"); // Allows up to 18 digits with 2 decimal places
+        }
+    }
+}
